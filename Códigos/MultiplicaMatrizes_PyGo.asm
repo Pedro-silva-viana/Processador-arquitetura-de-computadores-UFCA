8b6015827c31ba68724638dd339947dcd6ce7c69
Alan Gabriel <alangabryel13@gmail.com>
2024-11-05 09:04:35 -0300

    Versão funcional do código no PyGo

--- Códigos/MultiplicaMatrizes_PyGo.asm
+++ Códigos/MultiplicaMatrizes_PyGo.asm
@@ -1,31 +1,30 @@
-pega £00, 4 _ pega £08, 0
-pega £01, 1 _ pega £09, 0
-pega £02, 16 _ pega £10, 32
+pega £00, 4 _ pega £08, 0           //tamanho do vetor, i  = 0
+pega £01, 1 _ pega £09, 0           //valor de incremento, Endereço da matriz A
+pega £02, 16 _ pega £10, 32         //Endereço da matriz B, Endereço da matriz C
 aMimir _ aMimir
-igual £08, £00, X _ igual £08, £00, X
-pega £03, 0 _ aMimir
-aMimir _ aMimir
+igual £08, £00, 22 _ pega £13, 0
 aMimir _ aMimir
 aMimir _ aMimir
 aMimir _ aMimir
-igual £00, £03, Y _ igual £00, £03, Y
+aMimir _ aMimir
+igual £00, £13, 11 _ aMimir
 aMimir _ aMimir
 aMimir _ aMimir
 aMimir _ aMimir
 fodaSe £04 _ fodaSe £11
 multiplica £05, £08, £00 _ multiplica £12, £08, £00
-somaFoda £05, £03 _ somaFoda £12, £03
+somaFoda £05, £13 _ somaFoda £12, £13
 somaFoda £05, £09 _ somaFoda £12, £02
 escreverFoda £04 _ aMimir
-soma £03, £03, £01 _ aMimir
+aMimir _ soma £13, £13, £01
 aMimir _ aMimir
-vai Y _ vai Y
+vai 8 _ aMimir
 escrever £11, £12 _ aMimir
 aMimir _ aMimir
 aMimir _ aMimir
 aMimir _ aMimir
 aMimir _ soma £08, £08, £01
-vai X _ vai X
+vai 3 _ aMimir
 aMimir _ aMimir
 aMimir _ aMimir
 aMimir _ aMimir
@@ -35,5 +34,48 @@ aMimir _ aMimir
 aMimir _ aMimir
 aMimir _ aMimir
 aMimir _ aMimir
-igual £08, £00, X _ igual £08, £00, X
-pega £03, 0 _ aMimir
+igual £08, £00, 40 _ pega £11, 0
+aMimir _ aMimir
+aMimir _ aMimir
+aMimir _ aMimir
+aMimir _ aMimir
+igual £11, £00, 30 _ pega £12, 0
+aMimir _ aMimir
+aMimir _ aMimir
+aMimir _ aMimir
+aMimir _ pega £13, 0
+igual £12, £00, 19 _ aMimir
+aMimir _ aMimir
+aMimir _ aMimir
+aMimir _ aMimir
+aMimir _ aMimir
+multiplica £03, £08, £00 _ multiplica £14, £12, £00
+somaFoda £03, £12 _ somaFoda £14, £11
+somaFoda £03, £09 _ somaFoda £14, £02
+lerFoda £03 _ aMimir
+aMimir _ aMimir
+aMimir _ aMimir
+aMimir _ aMimir
+ler £04, £14 _ aMimir
+aMimir_ soma £12, £12, £01
+aMimir _ aMimir
+aMimir _ aMimir
+aMimir _ aMimir
+aMimir _ aMimir
+vai 46 _ multiplica £14, £04, £03 
+aMimir _ somaFoda £13, £13
+aMimir _ aMimir
+multiplica £03, £08, £00 _ aMimir
+somaFoda £03, £11 _ aMimir
+somaFoda £03, £10 _ soma £11, £11, £01
+escreverFoda £13 _ aMimir
+vai 41 _ aMimir
+aMimir _ aMimir
+aMimir _ aMimir
+aMimir _ aMimir
+aMimir _ soma £08, £08, £01
+vai 36 _ aMimir
+aMimir _ aMimir
+aMimir _ aMimir
+aMimir _ aMimir
+aMimir _ aMimir
